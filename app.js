f778c00f30f7631d743864197f178d3e6f21afb4
lucasteixeira <prilosac@gmail.com>
2018-05-28 14:39:27 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var path = require('path');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var srvPort = 3000;
 
 var app = express();
 
-app.listen(3000, function(){
-	console.log('Server started on port 3000...');
+//View Engine (let server know where views will be)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set Static Folder (place for all the Angular stuff to live)
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', tasks);
+
+app.listen(srvPort, function(){
+	console.log('Server started on port ' + srvPort + '...');
 })
