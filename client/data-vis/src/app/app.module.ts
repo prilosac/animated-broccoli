42f04e89964b35e786f91aadae0f44e6b6203dee
Lucas Teixeira <prilosac@gmail.com>
2018-05-28 23:06:19 +0000

    fixing HomeComponent

--- client/data-vis/src/app/app.module.ts
+++ client/data-vis/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
