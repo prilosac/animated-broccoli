190bf5b467472a611e8e5eaa587d6d00e8fd07e7
lucasteixeira <prilosac@gmail.com>
2018-05-28 15:09:22 -0500

    barebones API set up

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://prilo:lucas5658@ds237700.mlab.com:37700/jobprep_prilo', ['tasks']);
 
 router.get('/tasks', function(req, res, next){
-	res.send('TASK API');
+	db.tasks.find(function(err, tasks){
+		if(err){
+			res.send(err);
+		}
+		res.json(tasks);
+	});
 });
 
 module.exports = router;
