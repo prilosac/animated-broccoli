0a10ce06186f18cdf7d53f257ad511fb63bb3c4d
lucasteixeira <prilosac@gmail.com>
2018-05-28 18:08:16 -0500

    still fixing home component

--- client/data-vis/src/app/app.module.ts
+++ client/data-vis/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { HomeComponentComponent } from './home-component/home-component.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
     HomeComponent
   ],
   imports: [
