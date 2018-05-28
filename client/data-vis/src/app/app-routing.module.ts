452c22d5e47c35db4b989a5fed787e1877ae189b
lucasteixeira <prilosac@gmail.com>
2018-05-28 18:03:50 -0500

    homecomponent work

--- client/data-vis/src/app/app-routing.module.ts
+++ client/data-vis/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home-component/home-component.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent
+	}
+
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [
+  	RouterModule
+  ]
   declarations: []
 })
 export class AppRoutingModule { }
