b2a0815b1c5af215e8447663a9a02b94b77409da
lucasteixeira <prilosac@gmail.com>
2018-05-30 16:34:53 -0500

    backend data response, hopefully

--- routes/dataProcess.js
+++ routes/dataProcess.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/process-svm', function(req, res, next){
-	response = [
-		{id: 1, value: "I'm a datum!"},
-		{id: 2, value: "But like, me too"}
-	];
+	response = {
+		id: 1, value: "I'm a datum!"
+	};
+		//{id: 2, value: "But like, me too"}
 	res.json(response);
 	//res.render('index.html');
 });
