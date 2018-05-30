fa37bdf5dbf0cfc46bef08ee96c730d304d00014
lucasteixeira <prilosac@gmail.com>
2018-05-30 16:29:42 -0500

    backend data response, hopefully

--- client/data-vis/src/app/services/data.service.ts
+++ client/data-vis/src/app/services/data.service.ts
@@ -11,7 +11,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getData(): Observable<Data[]> {
+  getData(): {
   	return(this.http.get('/api/process-svm'))
   	//return of(MockData);
   }
