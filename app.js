832dae7a49203e333d477ceed42750308d891918
lucasteixeira <prilosac@gmail.com>
2018-05-30 17:31:34 -0500

    backend data response, hopefully

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', index);
 app.use('/api', dataProcess);
 app.use(function(res, req, next){
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+	res.setHeader('Access-Control-Allow-Origin', 'http://138.68.20.168:4200');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
 	next();
 })
