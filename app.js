0efd5d7a0091485014e545b94f7e4d9686ca955c
lucasteixeira <prilosac@gmail.com>
2018-05-30 17:47:20 -0500

    backend data response, hopefully

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 var path = require('path');
 
@@ -15,12 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.use(function(res, req, next){
-	res.header('Access-Control-Allow-Origin', '*');
-	// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT');
-	// res.setheader('Access-Control-Allow-Headers', 'Content-Type');
-	next();
-})
+app.use(cors());
 
 // Set Static Folder (place for all the Angular stuff to live)
 app.use(express.static(path.join(__dirname, 'client/data-vis/dist')));
