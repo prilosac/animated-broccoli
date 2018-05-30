12839f52a154a0d8b22f96dd96aa7093db260d0b
lucasteixeira <prilosac@gmail.com>
2018-05-30 16:20:59 -0500

    backend data response, hopefully

--- client/data-vis/src/app/app.module.ts
+++ client/data-vis/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTPClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -8,7 +9,7 @@ import { RandomForestComponent } from './random-forest/random-forest.component';
 import { SVMComponent } from './svm/svm.component';
 import { BaggingComponent } from './bagging/bagging.component';
 import { AboutComponent } from './about/about.component';
-import { HTTPClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
