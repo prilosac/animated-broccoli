3a9c253d3dfcb4c59736a65ad626d0c2b9bda0ee
lucasteixeira <prilosac@gmail.com>
2018-05-30 16:25:54 -0500

    backend data response, hopefully

--- client/data-vis/src/app/services/data.service.ts
+++ client/data-vis/src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HTTPClient } from '@angular/http'
+import { HttpClient } from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 import { Data } from '../data';
 import { MockData } from '../mock-data';
